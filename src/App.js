f150d76cf3c106e5e719d08daab81665285fc9dc
Himanshunitrr <himanshumauryacrz@gmail.com>
2021-05-21 18:48:37 +0530

    added some styles

--- src/App.js
+++ src/App.js
@@ -1,44 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import Post from './Post'
-import Pagination from './Pagination'
+import React, { useState, useEffect } from "react";
+import Post from "./Post";
+import Pagination from "./Pagination";
+import "./App.css";
+
 export default function App() {
   const [posts, setPosts] = useState([]);
-  const [error, setError] = useState('');
-  const [b_name, setBank] = useState('');
+  const [error, setError] = useState("");
+  const [b_name, setBank] = useState("");
   const [page_limit, setPageLimit] = useState(5);
   const [data_limit, setDataLimit] = useState(3);
-  const [param, setParam] = useState('DELHI');
-  
+  const [param, setParam] = useState("DELHI");
+
   useEffect(() => {
-    const url = 'https://vast-shore-74260.herokuapp.com/banks?city=' + param
+    const url = "https://vast-shore-74260.herokuapp.com/banks?city=" + param;
     fetch(url)
       .then((response) => {
         if (response.ok) {
           return response.json();
         }
-        throw new Error('something went wrong while requesting posts');
+        throw new Error("something went wrong while requesting posts");
       })
-      .then(
-        (posts_mod) => {
-          posts_mod.map(bank => bank.bool = false)
-          setPosts(posts_mod)
-        }
-      )
+      .then((posts) => {
+        setPosts(posts);
+      })
       .catch((error) => setError(error.message));
   }, [param]);
   if (error) return <h1>{error}</h1>;
 
-  return <div>
-    <label for="city">Choose a city:</label>
-      <select name="city" id="city" onClick={(e) => setParam(e.target.value)}>
-        <option value="DELHI" >Delhi</option>
-        <option value="MUMBAI">Mumbai</option>
-    </select>
-    <label for="bank">Search a bank:</label>
-    <input type="text" name="bank" id="bank" value={b_name} onChange={e => setBank(e.target.value)} />
-    
-  
-    {posts.length > 0 ? (
+  return (
+    <div>
+      <header>
+        <div>
+          <label for="city">Choose a city:</label>
+          <select
+            name="city"
+            id="city"
+            onClick={(e) => setParam(e.target.value)}
+          >
+            <option value="DELHI">Delhi</option>
+            <option value="MUMBAI">Mumbai</option>
+            <option value="CHENNAI">CHENNAI</option>
+            <option value="KOLKOTA">KOLKOTA</option>
+            <option value="RAIPUR">RAIPUR</option>
+          </select>
+        </div>
+        <div>
+          <label for="bank">Search a bank:</label>
+          <input
+            type="text"
+            name="bank"
+            id="bank"
+            value={b_name}
+            onChange={(e) => setBank(e.target.value)}
+          />
+        </div>
+      </header>
+      {posts.length > 0 ? (
         <>
           <Pagination
             data={posts}
@@ -46,11 +63,12 @@ export default function App() {
             title="Banks"
             pageLimit={page_limit}
             dataLimit={data_limit}
-          b_name={b_name.toUpperCase()}
+            b_name={b_name.toUpperCase()}
           />
         </>
       ) : (
-       <h1>Loading</h1>
+        <h1>Loading</h1>
       )}
-  </div>;
+    </div>
+  );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,22 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 export default function Post(props) {
-  let setFiltered = props.setFiltered
-  let filtered = props.filtered
-  const { branch, bank_name, address} = props.data;
+  let setFiltered = props.setFiltered;
+  let filtered = props.filtered;
+  const { branch, bank_name, address } = props.data;
 
   return (
     <div className="post">
-    <h1>{bank_name}</h1>
-    <small>{branch}</small>
+      <h1>{bank_name}</h1>
+      <small>{branch}</small>
       <p>{address}</p>
-      <button onClick={(e, filtered) => setFiltered(filtered => [...filtered, props.data])}>Fav</button>
+      <button
+        onClick={(e, filtered) =>
+          setFiltered((filtered) => [...filtered, props.data])
+        }
+      >
+        Fav
+      </button>
     </div>
   );
 }
